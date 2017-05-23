ed4b05205b10c3e89eb591113478e1838898e130
techdecline <cornelius.schuchardt@gmail.com>
2017-05-23 11:53:38 +0200

    Add files via upload

---
+++ Mod1_NanoServer.ps1
@@ -0,0 +1,37 @@
+﻿# Module 1 Example Code - Create Nano Server with ODJ File
+#################################################
+
+# 0. Variables
+##############
+
+$dcName = "CM-DC1"
+$nanoName = "CM-Nano1"
+$basePath = "C:\NanoServer"
+$mediaPath = "J:\"
+$securePassword = ConvertTo-SecureString -AsPlainText "Pa55w.rd" -Force
+$switchName = "Private Network"
+
+# 1. Generate Blob File
+#######################
+
+Invoke-Command -ComputerName $dcName -ArgumentList $nanoName -ScriptBlock {
+    param ($NanoName)
+    djoin.exe /provision /domain decline.lab /machine $NanoName /savefile "$env:windir\Temp\$NanoName.blob"
+}
+
+# 2. Load Nano Server Module
+############################
+
+Import-Module "$basePath\NanoServerImageGenerator\NanoServerImageGenerator.psm1"
+
+# 3. Create Nano Server Image
+#############################
+
+New-NanoServerImage -DeploymentType Guest -Edition Datacenter -MediaPath $mediaPath -BasePath $basePath `
+    -TargetPath (Join-Path -Path $basePath -ChildPath "$nanoName.vhdx") -Storage -Compute `
+    -DomainBlobPath "\\$dcName\c$\Windows\Temp\$nanoName.blob" -AdministratorPassword $securePassword
+
+# 4. Create Nano VM (requires Nested Virtualization)
+####################################################
+
+New-VM -Name $nanoName -MemoryStartupBytes 2GB -VHDPath (Join-Path -Path $basePath -ChildPath "$nanoName.vhdx") -Generation 2 `    -SwitchName $switchName
---
+++ Mod2_iSCSI.ps1
@@ -0,0 +1,21 @@
+﻿# Module 2 Example Code - iSCSI
+#################################
+
+# 1. Install iSCSI Target Server
+################################
+
+Install-WindowsFeature -Name FS-iSCSITarget-Server -IncludeAllSubFeature -IncludeManagementTools -Restart
+
+# 2. Create iSCSI Target
+########################
+
+$initiatorArr = "IPAddress:172.16.0.11","IPAddress:172.16.0.12"
+$target = New-IscsiServerTarget -TargetName SampleTarget -InitiatorIds $initiatorArr
+
+# 3. Create Disks and attach to target
+$intArr = 1..3
+
+foreach ($int in $intArr)
+{
+    New-IscsiVirtualDisk -Path "C:\iscsiDisk$int.vhdx" -SizeBytes 5GB | Add-IscsiVirtualDiskTargetMapping -TargetName $target.TargetName
+}
